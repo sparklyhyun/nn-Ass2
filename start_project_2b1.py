2ddf479eb45db0cc3d83fb75a08e36c8b439b0bd
hyun\Hyun Jeong <LIMH0141@e.ntu.edu.sg>
2018-10-29 00:20:46 +0800

    dimension problems

--- start_project_2b1.py
+++ start_project_2b1.py
@@ -10,101 +10,143 @@ FILTER_SHAPE2 = [20,1]
 POOLING_WINDOW = 4
 POOLING_STRIDE = 2
 MAX_LABEL = 15
+batch_size = 128
 
-no_epochs = 10 #originally 100
+no_epochs = 10  #originally 100
 lr = 0.01
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 seed = 10
 tf.set_random_seed(seed)
 
+#cnn model 
 def char_cnn_model(x):
-  
-  input_layer = tf.reshape(
-      tf.one_hot(x, 256), [-1, MAX_DOCUMENT_LENGTH, 256, 1])
+    input_layer = tf.reshape(tf.one_hot(x, 256), [-1, MAX_DOCUMENT_LENGTH, 256, 1])
 
-  with tf.variable_scope('CNN_Layer1'):
-    conv1 = tf.layers.conv2d(
-        input_layer,
-        filters=N_FILTERS,
-        kernel_size=FILTER_SHAPE1,
-        padding='VALID',
-        activation=tf.nn.relu)
-    pool1 = tf.layers.max_pooling2d(
-        conv1,
-        pool_size=POOLING_WINDOW,
-        strides=POOLING_STRIDE,
-        padding='SAME')
+    #convolutional & pooling layer 1
+    with tf.variable_scope('CNN_Layer1'):
+        conv1 = tf.layers.conv2d(
+            input_layer,
+            filters=N_FILTERS,
+            kernel_size=FILTER_SHAPE1,
+            padding='VALID',
+            activation=tf.nn.relu)
+        pool1 = tf.layers.max_pooling2d(
+            conv1,
+            pool_size=POOLING_WINDOW,
+            strides=POOLING_STRIDE,
+            padding='SAME')
 
-    pool1 = tf.squeeze(tf.reduce_max(pool1, 1), squeeze_dims=[1])
+        #pool1 = tf.squeeze(tf.reduce_max(pool1, 1), squeeze_dims=[1])
 
-  logits = tf.layers.dense(pool1, MAX_LABEL, activation=None)
+    #convolutional & pooling layer 2
+    with tf.variable_scope('CNN_Layer2'):
+        conv2 = tf.layers.conv2d(
+            pool1,
+            filters = N_FILTERS,
+            kernel_size = FILTER_SHAPE2,
+            padding='VALID',
+            activation = tf.nn.relu)
+        pool2 = tf.layers.max_pooling2d(
+            conv2,
+            pool_size = POOLING_WINDOW,
+            strides = POOLING_STRIDE,
+            padding = 'SAME')
 
-  return input_layer, logits
+    pool2 = tf.squeeze(tf.reduce_max(pool2, 1), squeeze_dims=[1])
 
+    #output softmax layer 
+    logits = tf.layers.dense(pool2, MAX_LABEL, activation=tf.nn.softmax)    
+    return input_layer, logits
 
+#data preprocessing 
 def read_data_chars():
-  
-  x_train, y_train, x_test, y_test = [], [], [], []
+    x_train, y_train, x_test, y_test = [], [], [], []
 
-  with open('train_medium.csv', encoding='utf-8') as filex:
-    reader = csv.reader(filex)
-    for row in reader:
-      x_train.append(row[1])
-      y_train.append(int(row[0]))
+    with open('train_medium.csv', encoding='utf-8') as filex:
+        reader = csv.reader(filex)
+        for row in reader:
+            x_train.append(row[1])
+            y_train.append(int(row[0]))
 
-  with open('test_medium.csv', encoding='utf-8') as filex:
-    reader = csv.reader(filex)
-    for row in reader:
-      x_test.append(row[1])
-      y_test.append(int(row[0]))
-  
-  x_train = pandas.Series(x_train)
-  y_train = pandas.Series(y_train)
-  x_test = pandas.Series(x_test)
-  y_test = pandas.Series(y_test)
-  
-  
-  char_processor = tf.contrib.learn.preprocessing.ByteProcessor(MAX_DOCUMENT_LENGTH)
-  x_train = np.array(list(char_processor.fit_transform(x_train)))
-  x_test = np.array(list(char_processor.transform(x_test)))
-  y_train = y_train.values
-  y_test = y_test.values
-  
-  return x_train, y_train, x_test, y_test
+    with open('test_medium.csv', encoding='utf-8') as filex:
+        reader = csv.reader(filex)
+        for row in reader:
+            x_test.append(row[1])
+            y_test.append(int(row[0]))
 
-  
+    x_train = pandas.Series(x_train)
+    y_train = pandas.Series(y_train)
+    x_test = pandas.Series(x_test)
+    y_test = pandas.Series(y_test)
+
+
+    char_processor = tf.contrib.learn.preprocessing.ByteProcessor(MAX_DOCUMENT_LENGTH)
+    x_train = np.array(list(char_processor.fit_transform(x_train)))
+    x_test = np.array(list(char_processor.transform(x_test)))
+    y_train = y_train.values
+    y_test = y_test.values
+
+    return x_train, y_train, x_test, y_test
+
+
 def main():
-  
-  x_train, y_train, x_test, y_test = read_data_chars()
 
-  print(len(x_train))
-  print(len(x_test))
+    x_train, y_train, x_test, y_test = read_data_chars()
 
-  # Create the model
-  x = tf.placeholder(tf.int64, [None, MAX_DOCUMENT_LENGTH])
-  y_ = tf.placeholder(tf.int64)
+    print(len(x_train))
+    print(len(x_test))
 
-  inputs, logits = char_cnn_model(x)
+    # Create the model
+    x = tf.placeholder(tf.int64, [None, MAX_DOCUMENT_LENGTH])
+    y_ = tf.placeholder(tf.int64)
 
-  # Optimizer
-  entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.one_hot(y_, MAX_LABEL), logits=logits))
-  train_op = tf.train.AdamOptimizer(lr).minimize(entropy)
+    inputs, logits = char_cnn_model(x)
 
-  sess = tf.Session()
-  sess.run(tf.global_variables_initializer())
+    # Optimizer
+    entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.one_hot(y_, MAX_LABEL), logits=logits))
+    train_op = tf.train.AdamOptimizer(lr).minimize(entropy)
 
-  # training
-  loss = []
-  for e in range(no_epochs):
-    _, loss_  = sess.run([train_op, entropy], {x: x_train, y_: y_train})
-    loss.append(loss_)
+    #prediction part 
+    correct_prediction = tf.cast(tf.equal(tf.argmax(logits, 1), tf.argmax(y_, 1)), tf.int64)
+    correct_prediction = tf.cast(correct_prediction, tf.int64)
+    accuracy = tf.reduce_mean(correct_prediction)
 
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
-    if e%1 == 0:
-      print('iter: %d, entropy: %g'%(e, loss[e]))
-  
-  sess.close()
+        # training
+        loss = []
+        loss_batch=[]
+        test_acc =[]
+
+        #breaking down into batches
+        N = len(x_train)
+        idx = np.arange(N)
+        
+        for e in range(no_epochs):
+            np.random.shuffle(idx)
+            trainX_batch, trainY_batch = x_train[idx], y_train[idx]
+            #batch training
+            for start, end in zip(range(0, N, batch_size), range(batch_size, N, batch_size)):
+                  #_, loss_  = sess.run([train_op, entropy], {x: trainX_batch[j], y_: trainY_batch[j]})
+                _, loss_  = sess.run([train_op, entropy], {x: trainX_batch[start:end], y_: trainY_batch[start:end]})
+                loss_batch.append(loss_)
+                  
+            loss.append(sum(loss_batch)/len(loss_batch))
+            loss_batch[:] = []
+            #test accuracy!!!!!!
+            test_acc.append(accuracy.eval(feed_dict ={x: x_test, y_: y_test}))
+
+            if e%1 == 0:
+                print('iter: %d, entropy: %g'%(e, loss[e]))
+                print('iter: %d, accuracy: %g'%(e, test_acc[e]))
+
+        #plot figures here
+
+        
+        sess.close()
 
 if __name__ == '__main__':
-  main()
+    main()
+
